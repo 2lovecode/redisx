7636be0fd795fd5bdb2a312244f89428d637f703
2lovecode <liu546hao@163.com>
2024-07-02 17:21:39 +0800

    a

---
+++ src/bin/server.rs
@@ -0,0 +1,85 @@
+use bytes::Bytes;
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
+
+const SHARE_NUM: usize = 10;
+
+#[tokio::main]
+async fn main() {
+    let listener  = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+
+
+    let mut db_vec = Vec::with_capacity(SHARE_NUM);
+
+    for _ in 0..SHARE_NUM {
+        db_vec.push(Mutex::new(HashMap::new()));
+    }
+
+    let db:Db = Arc::new(db_vec);
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        let db = db.clone();
+        tokio::spawn(async {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db : Db) {
+    let mut conn = Connection::new(socket);
+    use mini_redis::Command::{self, Get, Set};
+
+    while let Some(frame) = conn.read_frame().await.unwrap() {
+        let res = match Command::from_frame(frame).unwrap() {
+            Get(cmd) => {
+                match string_to_int_hash(cmd.key()) {
+                    Ok(hash_value) => {
+                        let shard = db[hash_value as usize % SHARE_NUM].lock().unwrap();
+                        match shard.get(cmd.key()) {
+                            Some(value) => {
+                                Frame::Bulk(value.clone().into())
+                            },
+                            None => {
+                                Frame::Null
+                            }
+                        }
+                    },
+                    Err(_) => {
+                        Frame::Null
+                    },
+                }
+            }
+            Set(cmd) => {
+                match string_to_int_hash(cmd.key()) {
+                    Ok(hash_value) => {
+                        let mut shard = db[hash_value as usize % SHARE_NUM].lock().unwrap();
+                        shard.insert(cmd.key().to_string(), cmd.value().clone());
+                        Frame::Simple("OK".to_string())
+                    },
+                    Err(_) => {
+                        Frame::Simple("OK".to_string())
+                    },
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd)
+        };
+
+        conn.write_frame(&res).await.unwrap();
+    }
+}
+
+
+fn string_to_int_hash(s: &str) -> Result<u64, std::num::ParseIntError> {
+    let num: i32 = s.parse()?;
+    let mut hasher = DefaultHasher::new();
+    num.hash(&mut hasher);
+    Ok(hasher.finish())
+}
